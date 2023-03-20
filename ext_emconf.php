52d6dc93afa78e7d50bb6eb9fc96239430f811f3
Markus Hofmann <m.hofmann@web-vision.de>
2023-03-20 14:05:23 +0100

    [TASK] Add dependencies

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => '(FGTCLB) Educational Package Basic',
+    'description' => 'Basic setup for FGTCLB Educational packages',
+    'category' => 'plugin',
+    'author' => '',
+    'author_email' => '',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
