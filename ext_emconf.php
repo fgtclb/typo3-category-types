f8175b586b5eb436b427876c26c3f703ec2d8b81
Stefan Bürk <stefan@buerk.tech>
2025-07-14 12:30:33 +0200

    [TASK] Raise lowest supported TYPO3 version constraint to `^12.4.22`
    To prepare for a required bugfix the lowest supported TYPO3
    version needs to be raised to `^12.4.22`[2] to ensure that
    the standalone fluid version is at least in `2.11.0`[1] or
    newer, for `composer` and `classic` mode based instances.
    
    To name it, `<f:split />` ViewHelper is required for the fix.
    
    The required lowest TYPO3 version has still quite a range up
    to the latest and it can be safely assumed or expected that
    instances upgrades to that version or higher and includes:
    
    * Adjust `composer.json` and `ext_emconf.php` constraints
      of all split packages.
    * Adjust `composer.json` of shared mono-repository dev
      package used for monorepo ddev instances.
    * Adjust `composer.json` and `ext_emconf.php` of test
      fixture extension(s).
    
    [1] https://github.com/TYPO3/Fluid/releases/tag/2.11.0
    [2] https://packagist.org/packages/typo3/cms-fluid#v12.4.22

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.22-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
