b63f960c8146a441ebb438be1db30ff9373ed29f
Stefan Bürk <stefan@buerk.tech>
2026-04-26 16:52:16 +0200

    [TASK] DPL-81: Streamline unit- and functional basic tests
    Each provided extension should have at least following
    basic tests:
    
    * Verify against supported TYPO3 core versions
      as unit and functional test.
    
    * Verify loaded extensions in tests it depends
      on as functional test.
    
    To make this maintanable in a simple way two generic
    traits are added:
    
    * `\FGTCLB\TestingHelper\FunctionalTestCase\ExtensionCoreVersionCompatTestsTrait`
    * `\FGTCLB\TestingHelper\FunctionalTestCase\ExtensionsLoadedTestsTrait`
    
    All `pacakges/*/*` are streamlined to provide following
    tests:
    
    * `Tests/Unit/VersionCompatTest.php`
    * `Tests/Functional/VersionCompatTest.php`
    * `Tests/Functional/ExtensionsLoadedTest.php`
    
    Functional tests uses the corresponding abstract testcase
    provided by that extension to share the default loaded
    extensions in the functional test instance.

---
+++ Tests/Unit/VersionCompareTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\CategoryTypes\Tests\Unit;
+
+use FGTCLB\TestingHelper\FunctionalTestCase\ExtensionCoreVersionCompatTestsTrait;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class VersionCompareTest extends UnitTestCase
+{
+    use ExtensionCoreVersionCompatTestsTrait;
+}
